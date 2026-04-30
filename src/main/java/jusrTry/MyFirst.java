0142e6756ddcf47d7001a696fef562f401803dec
kalpesh.chavhan <kalpeshchavhan1234@gmail.com>
2026-04-30 12:27:37 +0530

    my comit

---
+++ src/main/java/jusrTry/MyFirst.java
@@ -0,0 +1,16 @@
+package jusrTry;
+
+import org.testng.annotations.Test;
+
+public class MyFirst {
+
+	
+	@Test
+	public void jenkinsDemo() {
+		
+		System.out.println("I am learning the jenkins");
+		
+		
+	}
+	
+}
